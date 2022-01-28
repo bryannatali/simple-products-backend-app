170df5c059ca19bc8e5e7943f37f176297328e60
Gregory <gregory.natali4@gmail.com>
2022-01-28 14:04:25 -0300

    task done

--- src/entities/Product.ts
+++ src/entities/Product.ts
@@ -2,4 +2,13 @@
 // Dica: Pesquisar como criar classes com Typescript
 
 export class Product {
+  name: string
+  description: string
+  price: number
+
+  constructor(n:string, d:string, p:number) {
+    this.name = n
+    this.description = d
+    this.price = p
+  }
 }
